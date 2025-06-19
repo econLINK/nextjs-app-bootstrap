40fd0f38e4110e0dc1978c21923f2e73f1300724
Edison Shyti <eagledigitalcloud@gmail.com>
2025-06-19 07:58:22 +0200

    Add .next/static/chunks/src_app_test_page_tsx_c28dd2e6._.js 1750312702787

---
+++ .next/static/chunks/src_app_test_page_tsx_c28dd2e6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_619d48a0._.js",
+  "static/chunks/src_be06555e._.js"
+],
+    source: "dynamic"
+});
